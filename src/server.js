ebc390ab3d2490dd1481e890ef0be0320dceaa27
Lucas de Andrade <bandradelucas@gmail.com>
2019-08-10 21:09:53 -0300

    Controllers, models and new packages (axios, cors, mongoose)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
 const routes = require('./routes')
 
 const server = express()
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@week-8-omnistack-wnx3n.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 

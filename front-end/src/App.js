c207987aa22bdd61dceb12c48c6b9b4a03e2475f
Lucas de Andrade <bandradelucas@gmail.com>
2019-08-11 02:27:24 -0300

    Front End: finalizado login, main page e estilizações.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
 
+import Routes from './routes'
+
 function App() {
-    return (
-        <h1>Hello Komorebi</h1>
-    )
+    return <Routes />
 }
 
 export default App

2569d7cb2774f0d65c9cc83e1d37b88439a52d01
Lucas de Andrade <bandradelucas@gmail.com>
2019-10-06 20:34:17 -0300

    Back-End thumbnail_url servidor de arquivos estáticos

--- week-9/back-end/src/models/Spot.js
+++ week-9/back-end/src/models/Spot.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
 
-const SpotSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+const SpotSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        thumbnail: String,
+        company: String,
+        price: Number,
+        techs: [String],
     },
-    thumbnail: String,
-    company: String,
-    price: Number,
-    techs: [String],
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
